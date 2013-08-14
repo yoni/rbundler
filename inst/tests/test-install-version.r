c87f0f45593d34f37cf4fd0173587786647528e2
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-08-14 00:37:39 -0400

    Update rbundler to be idempotent when it comes to re-installing the same package version, but error out if a package is already installed but has the wrong version.

--- inst/tests/test-install-version.r
+++ inst/tests/test-install-version.r
@@ -4,19 +4,34 @@ repos <- 'http://cran.rstudio.com'
 options(repos=repos)
 options(pkgType = 'source')
 
-old_versions <- c('1', '2')
-available_versions <- c(old_versions, '3')
+dependency <- mock_dependency(repos=repos)
 
-expect_version_to_install <- function(expected, version, comparator) {
-  version_to_install <- determine_version_to_install(available_versions, version, comparator)
-  expect_equal(
-    version_to_install,
-    expected,
-    info = sprintf('Expected version [%s] but got [%s]', expected, paste(version_to_install, collapse=','))
-  )
+create_new_lib <- function() {
+  lib <- file.path(tempdir(), 'library', as.numeric(Sys.time()))
+
+  dir.create(lib, recursive=TRUE)
+  .libPaths(lib)
+  lib
 }
 
+test_that('can compare an available version with a requested version', {
+  expect_true(compare_versions('1', '==', '1'))
+  expect_false(compare_versions('2', '==', '1'))
+})
+
 test_that('the correct version for installation can be determined',{
+
+  old_versions <- c('1', '2')
+  available_versions <- c(old_versions, '3')
+  expect_version_to_install <- function(expected, version, comparator) {
+    version_to_install <- determine_version_to_install(available_versions, version, comparator)
+    expect_equal(
+      version_to_install,
+      expected,
+      info = sprintf('Expected version [%s] but got [%s]', expected, paste(version_to_install, collapse=','))
+    )
+  }
+
 
   for(version in available_versions) {
     expect_version_to_install('3', version, NA)
@@ -48,12 +63,7 @@ test_that('can read archive from both cran and non-cran-like repsitories', {
 
 test_that('can install an old package version', {
 
-  # Create a temporary library to use for testing:
-  lib <- file.path(tempdir(), 'library', as.numeric(Sys.time()))
-
-  dir.create(lib, recursive=TRUE)
-  .libPaths(lib)
-  dependency <- mock_dependency(repos=repos)
+  lib <- create_new_lib()
   install_version(dependency$name, dependency$version, '==')
 
   expected_path <- file.path(lib, dependency$name)
@@ -64,3 +74,24 @@ test_that('can install an old package version', {
 
 })
 
+test_that('install_version is idempotent', {
+
+  create_new_lib()
+  # Try to install the package several times, each time with a compatible version:
+  expect_true(install_version(dependency$name, dependency$version, '=='))
+  expect_false(install_version(dependency$name, dependency$version, '=='))
+  expect_false(install_version(dependency$name, dependency$version, '>='))
+  expect_false(install_version(dependency$name, dependency$version, '<='))
+
+})
+
+test_that('install_version throws an exception if a package has already been installed but has the wrong version', {
+
+  create_new_lib()
+  # Try to install the package several times. The first time with a valid version, and subsequent times with invalid
+  # compatibility:
+  expect_true(install_version(dependency$name, dependency$version, '=='))
+  expect_error(install_version(dependency$name, dependency$version, '<'))
+  expect_error(install_version(dependency$name, dependency$version, '>'))
+
+})
